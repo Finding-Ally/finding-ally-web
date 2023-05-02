8f17fe143f9b3175a5bcd371796fe4698f4fbbeb
Ayush Vidhale <ayushvidhale099@gmail.com>
2023-05-02 19:09:41 +0530

    landing page

--- pages/join.jsx
+++ pages/join.jsx
@@ -2,37 +2,17 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
-import { BsGithub, BsTwitter, BsGoogle } from "react-icons/bs";
+
+import Hero from "../components/hero";
+import Features from '@/components/features'
+import FeaturesBlocks from '@/components/features-blocks'
+import Testimonials from '@/components/testimonials'
+import Newsletter from '@/components/newsletter'
+import Header from '@/components/ui/header'
+import Footer from '@/components/ui/footer'
 
 const Signin = () => {
-  const { data: session, status } = useSession();
-  const { push } = useRouter();
-  const [email, setEmail] = useState("");
-
-  console.log(session);
-  if (status === "loading")
-    return (
-      <div className="flex flex-col place-content-center justify-center place-items-center w-full h-screen pb-24 overflow-auto text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
-        <h1>Checking Authentication</h1>
-      </div>
-    );
-
-  if (session) {
-    setTimeout(() => {
-      push("/");
-    }, 5000);
-
-    return (
-      <div className="flex flex-col place-content-center justify-center place-items-center w-full h-screen pb-24 overflow-auto text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
-        <button type="button" class="bg-indigo-500 rounded-2xl p-2" disabled>
-          <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
-          Redirecting to Dashboard...
-        </button>
-      </div>
-    );
-  }
-
-  const handleOAuthSignIn = (provider) => () => signIn(provider);
+  
 
   // const handleSubmit = (e) => {
   // 	e.preventDefault()
@@ -41,23 +21,19 @@ const Signin = () => {
 
   // 	signIn('email', { email, redirect: false })
   // }
+ 
 
+  
   return (
-    <div className="flex flex-col place-content-center justify-center place-items-center w-full h-screen pb-24 overflow-auto text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
-      {/* <form onSubmit={handleSubmit}>
-				<label>Email Address</label>
-				<input
-					value={email}
-					type='email'
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-
-				<button type='submit' w='100%' my={5}>
-					Login
-				</button>
-			</form> */}
-
-      <div>
+    <>
+          <div className="flex flex-col place-content-center justify-center place-items-center w-full overflow-x-hidden text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
+          <Header />
+      <Hero />
+      <Features />
+      <FeaturesBlocks />
+      <Testimonials />
+      <Newsletter />
+      {/* <div>
         <button
           className="bg-white hover:bg-gray-200 border-2 border-gray-300 rounded-md shadow-md p-2 m-2"
           onClick={handleOAuthSignIn("google")}
@@ -67,8 +43,10 @@ const Signin = () => {
           <FcGoogle className="inline-block mr-2 text-4xl" />
           Continue with google
         </button>
-      </div>
+      </div> */}
+      <Footer />
     </div>
+    </>
   );
 };
 
