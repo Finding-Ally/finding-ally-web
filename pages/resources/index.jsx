bb11cad25224515b2fd93b3a9e6b3ed291b63a58
Ayush Vidhale <ayushvidhale099@gmail.com>
2023-06-03 13:56:27 +0530

    resources page

--- pages/resources/index.jsx
+++ pages/resources/index.jsx
@@ -1,6 +1,7 @@
 import { useSession } from "next-auth/react";
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
 export default function Resources(){
@@ -8,15 +9,39 @@ export default function Resources(){
     const {data: session} = useSession();
     console.log(session);
 
+
+    // Initialize a state to store the fetched data
+  const [currentUserRooms, setCurrentUserRooms] = useState([]);
+
+  // Fetch data from MongoDB using useEffect
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `/api/newroom?userId=${session?.user?.id}`
+        );
+        const jsonData = await response.json();
+        setCurrentUserRooms(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+  }, [session?.user?.id]);
+
+
+
     return (
         <div className="w-full pl-[87px] h-screen pb-24 overflow-auto text-gray-700 bg-gradient-to-r bg-purple-300 from-10% to-blue-300 to-90%">
 
-      
-      <div className="px-10 mt-4">
+<div className="px-10 mt-4">
         <div className="flex items-center flex-shrink-0 h-10">
           <span className="block text-xl font-bold">Your Ally Resources</span>
           <span className="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30">
-            3
+            {currentUserRooms.length || 0}
           </span>
           <Link href="/explore" className="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100">
             <svg
@@ -26,9 +51,9 @@ export default function Resources(){
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 6v6m0 0v6m0-6h6m-6 0H6"
               ></path>
             </svg>
@@ -36,10 +61,13 @@ export default function Resources(){
         </div>
         <div className="grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-4 pb-2 overflow-auto">
 
-          <div
+
+          {currentUserRooms.length > 0 ? currentUserRooms.map((room) => ( 
+
+          <Link href={`/resources/${room?.name}`} key={room?._id}
             className="relative flex flex-col items-start p-4 py-4 mt-3 bg-white rounded-lg bg-opacity-90 group hover:bg-opacity-100"
           >
-            <button className="absolute top-0 right-0 items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex">
+            {/* <button className="absolute top-0 right-0 items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex">
               <svg
                 className="w-4 h-4 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
@@ -48,23 +76,22 @@ export default function Resources(){
               >
                 <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
               </svg>
-            </button>
+            </button> */}
             <span className="text-lg font-bold mt-2 ">
-              Room Name
+              {room?.name}
             </span>
             <h4 className="mt-1 text-sm font-medium mb-2">
-              This is the title of the card for the thing that needs to be done.
+              {room?.goal}
             </h4>
-            <div className="flex justify-between items-center mb-2 w-full mt-3 text-xs font-medium text-gray-400">
+            <div className="flex justify-between items-center mb-2 w-full mt-3 text-xs font-medium text-gray-400 pt-6">
             <div className="flex">
-              <img
+              {room?.members?.map((member) => (
+                <img key={member?._id}
                 className="w-8 h-8 ml-auto rounded-full"
-                src="https://randomuser.me/api/portraits/women/26.jpg"
+                src={member?.image}
               />
-              <img
-                className="w-8 h-8 ml-auto rounded-full"
-                src="https://randomuser.me/api/portraits/women/26.jpg"
-              />
+              )) 
+              }
               </div>
               <div className="flex items-center ">
                 <svg
@@ -74,9 +101,9 @@ export default function Resources(){
                   fill="currentColor"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
                 <span className="ml-1 leading-none">Dec 12</span>
@@ -84,114 +111,38 @@ export default function Resources(){
               
               
             </div>
+          </Link>
+
+          )): <div className="relative flex flex-col items-start p-4 py-8 mt-3 bg-white rounded-lg bg-opacity-90 group hover:bg-opacity-100"
+        >
+          {/* <button className="absolute top-0 right-0 items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex">
+            <svg
+              className="w-4 h-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+            </svg>
+          </button> */}
+          <span className="text-lg font-bold mt-2 ">
+            No Rooms Found
+          </span>
+          <h4 className="mt-1 text-sm font-medium mb-2">
+            Create a room to get started
+          </h4>
+          <div className="flex justify-between items-center mb-2 w-full mt-3 text-xs font-medium text-gray-400">
+          
+            
           </div>
-
-
-
-          <div
-            className="relative flex flex-col items-start p-4 py-4 mt-3 bg-white rounded-lg bg-opacity-90 group hover:bg-opacity-100"
-          >
-            <button className="absolute top-0 right-0 items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex">
-              <svg
-                className="w-4 h-4 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-              </svg>
-            </button>
-            <span className="text-lg font-bold mt-2 ">
-              Room Name
-            </span>
-            <h4 className="mt-1 text-sm font-medium mb-2">
-              This is the title of the card for the thing that needs to be done.
-            </h4>
-            <div className="flex justify-between items-center mb-2 w-full mt-3 text-xs font-medium text-gray-400">
-            <div className="flex">
-              <img
-                className="w-8 h-8 ml-auto rounded-full"
-                src="https://randomuser.me/api/portraits/women/26.jpg"
-              />
-              <img
-                className="w-8 h-8 ml-auto rounded-full"
-                src="https://randomuser.me/api/portraits/women/26.jpg"
-              />
-              </div>
-              <div className="flex items-center ">
-                <svg
-                  className="w-4 h-4 text-gray-300 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <span className="ml-1 leading-none">Dec 12</span>
-              </div>
-              
-              
-            </div>
-          </div>
-
-
-          <div
-            className="relative flex flex-col items-start p-4 py-4 mt-3 bg-white rounded-lg bg-opacity-90 group hover:bg-opacity-100"
-          >
-            <button className="absolute top-0 right-0 items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex">
-              <svg
-                className="w-4 h-4 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-              </svg>
-            </button>
-            <span className="text-lg font-bold mt-2 ">
-              Room Name
-            </span>
-            <h4 className="mt-1 text-sm font-medium mb-2">
-              This is the title of the card for the thing that needs to be done.
-            </h4>
-            <div className="flex justify-between items-center mb-2 w-full mt-3 text-xs font-medium text-gray-400">
-            <div className="flex">
-              <img
-                className="w-8 h-8 ml-auto rounded-full"
-                src="https://randomuser.me/api/portraits/women/26.jpg"
-              />
-              <img
-                className="w-8 h-8 ml-auto rounded-full"
-                src="https://randomuser.me/api/portraits/women/26.jpg"
-              />
-              </div>
-              <div className="flex items-center ">
-                <svg
-                  className="w-4 h-4 text-gray-300 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <span className="ml-1 leading-none">Dec 12</span>
-              </div>
-              
-              
-            </div>
-          </div>
+        </div>}
 
 
           
         </div>
+
+      </div>
+      <div className="px-10 mt-4">
 
 
         <br />
@@ -217,7 +168,7 @@ export default function Resources(){
             <h4 className="mt-1 text-sm font-medium mb-2">
               {session?.user?.major}
             </h4>
-            <div className="flex justify-between items-center mb-2 w-full mt-3 text-xs font-medium text-gray-400">
+            <div className="flex justify-between items-center mb-2 w-full mt-3 text-xs font-medium text-gray-400 pt-6">
             <div className="flex">
               <img
                 className="w-8 h-8 ml-auto bg-gray-300 rounded-full"
