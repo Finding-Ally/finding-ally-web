90c3a32d7410cbd97dbb5c1e3cad7d1976e498df
Ayush Vidhale <ayushvidhale099@gmail.com>
2023-06-07 09:41:59 +0530

    loading animation

--- pages/explore.jsx
+++ pages/explore.jsx
@@ -421,7 +421,7 @@ export default function Explore() {
     data-modal-target="authentication-modal"
     data-modal-toggle="authentication-modal"
     type="button"
-    className="absolute top-1/2 z-10 left-1/4 translate-x-5 translate-y-4 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+    className="absolute top-1/2 z-10 left-1/4 translate-x-5 translate-y-20 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
   >
     <span className="px-5 py-2.5 transition-all duration-75 bg-green-400 font-bold rounded-md group-hover:bg-opacity-0">
       FIND ALLY
